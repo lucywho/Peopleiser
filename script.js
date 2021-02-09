3338debe94606030def0d0944b224ba75e834886
Lucy Toman <lucy@toman.me.uk>
2021-02-09 17:51:47 +0100

    adds capacity and image to results, adds image file

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 console.log("script connected", $);
-let items = [
+let vehicles = [
     { name: "car", capacity: 5, image: "assets/car.jpg" },
     { name: "bus", capacity: 60, image: "assets/berlinbus.png" },
-    { name: "boeing747", capacity: 500, image: "assets/boeing747.jpg" },
-    { name: "OlympiaStadion", capacity: 75000, image: "🏟️" },
+    { name: "Boeing747", capacity: 500, image: "assets/boeing747.jpg" },
+    { name: "OlympiaStadion", capacity: 75000, image: "assets/olympstad.jpg" },
 ];
 
 function calcCapacity() {
-    let user_num = $("#usernum").val();
-    let user_item = $("#useritem").val();
+    let people = $("#usernum").val();
+    let vehicle = $("#useritem").val();
+    let usercapacity = vehicles.find((item) => item.name === vehicle).capacity;
+    let vPic = vehicles.find((item) => item.name === vehicle).image;
 
     $(".display-modal").addClass("vis");
 
-    if (!user_num || user_num == 0) {
+    if (!people || people == 0) {
         $(".results").html(`please choose a number`);
     } else {
-        $(".results").html(`you chose ` + user_num + " " + user_item);
+        $(".results").html(
+            `you chose ` +
+                people +
+                " " +
+                vehicle +
+                " which has a capacity of " +
+                usercapacity
+        );
+
+        let wide = people * 100;
+        $(".pic")
+            .css("width", wide + "px")
+            .html('<img src = "' + vPic + '" />');
     }
 }
 
